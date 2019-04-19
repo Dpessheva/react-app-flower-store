012f007ba99ea4db9e707c2956305276ae20c2dd
DPesheva <dpessheva@gmail.com>
2019-04-19 19:55:35 +0300

    add auth component

---
+++ flowers-store/src/components/auth/LoginPage.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react'
+import loginValidator from '../../utils/loginValidator'
+import toastr from 'toastr'
+import Input from '../common/Input';
+import Auth from '../../utils/auth'
+import {loginValidationFunc} from '../../utils/formValidator'
+import {loginAction, redirectAction} from '../../actions/authActions'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+
+class LoginPage extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  componentWillMount () {
+    if (Auth.isUserAuthenticated()) {
+      this.props.history.push('/')
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.loginError.hasError) {
+      toastr.error(nextProps.loginError.message)
+    } else if (nextProps.loginSuccess) {
+      this.props.redirect()
+      toastr.success('Login successful')
+      this.props.history.push('/')
+    }
+  }
+ 
+  onChange (e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  onSubmit (e) {
+    e.preventDefault()
+    if (!loginValidator(this.state.email, this.state.password)) return
+    this.props.login(this.state.email, this.state.password)
+  }
+ 
+  render () {
+    let validObj = loginValidationFunc(
+      this.state.email,
+      this.state.password
+    )
+
+    return (
+      <div className='form-wrapper'>
+        <h1>Login</h1>
+        <form onSubmit={this.onSubmit}>
+              <Input
+                type='text'
+                name='email'
+                label='E-mail'
+                placeholder='Enter e-mail'
+                value={this.state.email}
+                onChange={this.onChange}
+                valid={validObj.validEmail} />
+              <Input
+                type='password'
+                name='password'
+                label='Password'
+                placeholder='Enter password'
+                value={this.state.password}
+                onChange={this.onChange}
+                valid={validObj.validPassword} />
+              <input type='submit' value='Login' />
+        </form>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps (state) {
+  return {
+    loginSuccess: state.login.success,
+    loginError: state.loginError
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    login: (email, password) => dispatch(loginAction(email, password)),
+    redirect: () => dispatch(redirectAction())
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LoginPage))
