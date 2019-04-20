88fb2f8f2872acb925bdc54721bb5977d1f83519
DPesheva <dpessheva@gmail.com>
2019-04-20 18:46:10 +0300

    add Footer

---
+++ flowers-store/src/components/common/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+
+
+const Footer = ()=> {
+
+    return (
+
+       <footer id="footer" className="page-footer mt-4">
+       © CandyWarehouse {new Date().getFullYear()}
+       </footer>
+
+    );
+
+}
+
+export default Footer;
