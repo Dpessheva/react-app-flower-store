86f3f9e257e69c5c31254d2ba6c3ef2799dc0bfe
DPesheva <dpessheva@gmail.com>
2019-04-20 22:53:05 +0300

    update

--- flowers-store/src/index.js
+++ flowers-store/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/site.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
+import '../node_modules/toastr/build/toastr.min.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import {BrowserRouter} from 'react-router-dom'
+import reducers from './reducers'
+import {createStore, applyMiddleware, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combineReducers(reducers), applyMiddleware(thunk, createLogger))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'))
+registerServiceWorker()
